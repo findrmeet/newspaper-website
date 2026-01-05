e445c5cea067355142d2583edf418b2c2a91ecb4
findrmeet <findrmeet@gmail.com>
2026-01-05 11:36:11 +0600

    sidebar optimization

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,68 +1,30 @@
 import Link from "next/link";
 
+const sidebarLinks = [
+  "All Bangla Newspapers",
+  "Bangladesh E-Paper",
+  "All Online Newspapers",
+  "All Local Newspapers",
+  "Bangladesh Division Newspapers",
+  "All Bangladeshi English Newspapers",
+  "All Bangla TV Channels News",
+  "All Bangladesh Share Bazer Newspapers",
+  "All West Bangla Newspapers Kolkata",
+  "All Assam Newspapers",
+];
+
 const Sidebar = () => {
   return (
-    <div className="rounded-xl border border-gray-300 p-2 gap-2 max-w-60 sm:flex flex-col hidden">
-      <Link
-        href="#"
-        className="text-black bg-gray-100  rounded-md py-2 px-3 cursor-pointer focus:bg-indigo-500 focus:text-white hover:bg-gray-200"
-      >
-        All Bangla Newspapers
-      </Link>
-      <Link
-        href="#"
-        className="text-black bg-gray-100  rounded-md py-2 px-3 cursor-pointer focus:bg-indigo-500 focus:text-white hover:bg-gray-200"
-      >
-        Bangladesh E-Paper
-      </Link>
-      <Link
-        href="#"
-        className="text-black bg-gray-100  rounded-md py-2 px-3 cursor-pointer focus:bg-indigo-500 focus:text-white hover:bg-gray-200"
-      >
-        All Online Newspapers
-      </Link>
-      <Link
-        href="#"
-        className="text-black bg-gray-100  rounded-md py-2 px-3 cursor-pointer focus:bg-indigo-500 focus:text-white hover:bg-gray-200"
-      >
-        All Local Newspapers
-      </Link>
-      <Link
-        href="#"
-        className="text-black bg-gray-100  rounded-md py-2 px-3 cursor-pointer focus:bg-indigo-500 focus:text-white hover:bg-gray-200"
-      >
-        Bangladesh Division Newspapers
-      </Link>
-      <Link
-        href="#"
-        className="text-black bg-gray-100  rounded-md py-2 px-3 cursor-pointer focus:bg-indigo-500 focus:text-white hover:bg-gray-200"
-      >
-        All Bangladeshi English Newspapers
-      </Link>
-      <Link
-        href="#"
-        className="text-black bg-gray-100  rounded-md py-2 px-3 cursor-pointer focus:bg-indigo-500 focus:text-white hover:bg-gray-200"
-      >
-        All Bangla TV Channels News
-      </Link>
-      <Link
-        href="#"
-        className="text-black bg-gray-100  rounded-md py-2 px-3 cursor-pointer focus:bg-indigo-500 focus:text-white hover:bg-gray-200"
-      >
-        All Bangladesh Share Bazer Newspapers
-      </Link>
-      <Link
-        href="#"
-        className="text-black bg-gray-100  rounded-md py-2 px-3 cursor-pointer focus:bg-indigo-500 focus:text-white hover:bg-gray-200"
-      >
-        All West Bangla Newspapers Kolkata
-      </Link>
-      <Link
-        href="#"
-        className="text-black bg-gray-100  rounded-md py-2 px-3 cursor-pointer focus:bg-indigo-500 focus:text-white hover:bg-gray-200"
-      >
-        All Assam Newspapers
-      </Link>
+    <div className="max-w-60 hidden flex-col gap-2 rounded-xl border border-gray-300 p-2 sm:flex">
+      {sidebarLinks.map((label) => (
+        <Link
+          key={label}
+          href="#"
+          className="cursor-pointer rounded-md bg-gray-100 px-3 py-2 text-black transition-colors hover:bg-gray-200 focus:bg-indigo-500 focus:text-white"
+        >
+          {label}
+        </Link>
+      ))}
     </div>
   );
 };
